9a1ab30cbaddbe55f919d8eb8c422e7f740ab944
Amaan7979 <88380417+Amaan7979@users.noreply.github.com>
2021-10-22 16:31:52 +0530

    Add files via upload

---
+++ Fruits_baskets.js
@@ -0,0 +1,43 @@
+status1="";
+objects=[];
+function setup() {
+    canvas=createCanvas(500,500);
+    canvas.center();
+    objectDetector=ml5.objectDetector('COCOSSD',modelLoaded);
+    document.getElementById("status").innerHTML="status : detecting objects";
+}
+function preload(){
+    img=loadImage("fruits_basket.jpg");
+}
+function draw(){
+    image(img,0,0,500,500);
+    if(status1!= ""){
+       for (let index = 0; index < objects.length; index++) {
+        fill("red");
+        stroke("red");
+        percent=floor(objects[index].confidence*100)
+        noFill();
+        text(objects[index].label+" "+ percent +"%",objects[index].x,objects[index].y);
+        rect(objects[index].x,objects[index].y,objects[index].width,objects[index].height);
+        document.getElementById("status").innerHTML="status : objects detected ";
+    } 
+}
+
+}
+function modelLoaded(){
+status1=true;
+console.log("Model is loaded");
+objectDetector.detect(img,got_results);
+}
+function got_results(error,results){
+if(error){
+    console.error(error);
+}
+else{
+    console.log(results);
+    objects=results;
+}
+}
+function go_back(){
+    window.location="index.html";
+}
---
+++ main.js
@@ -0,0 +1,12 @@
+
+function bedroom(){
+    window.location="bedroom.html"
+}
+
+function playground(){
+    window.location="playground.html"
+}
+
+function Fruits_basket(){
+    window.location="Fruits_basket.html"
+}
